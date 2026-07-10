bcd7d8066a7fe07eb81941d8582f95bdec4c2541
MUDROis <mudro.is@yandex.ru>
2026-07-10 20:17:08 +0300

    значок для приложения

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'task-tracker-v1';
+const CACHE_NAME = 'task-tracker-v2';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './app.js',
     './manifest.json',
+    './grifon.png',
+    './grifon.ico',
     'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
 ];
 
